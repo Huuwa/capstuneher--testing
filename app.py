b66d57b52d1f2ab009c83b6c3e7b21ea8ee0fc82
Hassan <hasan@uxberts-MacBook-Pro.local>
2023-08-09 09:14:59 +0300

    should fix every problem possible with gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import joblib
 from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
 
@@ -25,8 +25,12 @@ def index():
     prediction = None
     if request.method == 'POST':
         text_input = request.form['text_input']
-        prediction = make_prediction(text_input)
+        if text_input:
+            prediction = make_prediction(text_input)
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    from gunicorn.app.wsgiapp import WSGIApplication
+    app_wsgi = WSGIApplication()
+    app_wsgi.app_uri = 'app:app'  # Assuming your Flask app is named 'app'
+    app_wsgi.run()
